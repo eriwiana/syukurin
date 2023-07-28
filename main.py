4dba76c5e2da5734264a6b3743be1026e495cacd
Eri Wiana <eriwiana@gmail.com>
2023-07-29 03:06:04 +0700

    Adjusted to docker compatibility using uvicorn

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+
+import uvicorn
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
@@ -9,8 +12,13 @@ app = FastAPI(
 
 register_tortoise(
     app,
-    db_url="sqlite://db.sqlite3",
+    db_url=os.environ.get("DB_URL", "sqlite://db.sqlite3"),
     modules={"models": ["models.base", "models.wedding"]},
     generate_schemas=True,
     add_exception_handlers=True,
 )
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, workers=1)
